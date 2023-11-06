5c60f9c5c20d0e5dfe49a81eee5325cd6a2d5315
EvgenySinyakin93 <136899756+EvgenySinyakin93@users.noreply.github.com>
2023-11-06 11:07:14 +0300

    Add files via upload

---
+++ DZ Setup and SAve/src/Main.java
@@ -0,0 +1,12 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+public class Main {
+    public static void main(String[] args) {
+//        var file = new File("C://Games/src");
+//        file.mkdir();
+//        var file1 = new File("C://Games/res");
+//        file1.mkdir();
+    }
+}
